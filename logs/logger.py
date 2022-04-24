70588233775337881cf1b55b747266f9e41ecd2d
mer006-2 <798481537@qq.com>
2022-04-24 16:14:50 +0800

    20220424

--- logs/logger.py
+++ logs/logger.py
@@ -1,9 +1,10 @@
 #! usr/bin/env python3
 # -*- coding:utf-8 -*-
+'''
 import os,sys
 parentdir = os.path.dirname(os.path.dirname(__file__))
 sys.path.insert(0,parentdir)
-
+'''
 import logging
 from config.conf import cm
 
@@ -11,15 +12,16 @@ class Log:
     def __init__(self):
         self.logger = logging.getLogger()
         if not self.logger.handlers:
-            self.logger.setLevel(logging.INFO)
+            #设置输出的level
+            self.logger.setLevel(logging.DEBUG)
 
-            #创建一个handle写入文件
+            #创建一个handle写入文件，并设置写入的level
             fh = logging.FileHandler(cm.log_file, encoding='utf-8')
-            fh.setLevel(logging.INFO)
+            fh.setLevel(logging.DEBUG)
 
-            #创建一个handle输出控制台
+            #创建一个handle输出控制台，并设置写入的level
             ch = logging.StreamHandler()
-            ch.setLevel(logging.INFO)
+            ch.setLevel(logging.DEBUG)
 
             #定义输入格式
             formatter = logging.Formatter(self.fmt)
@@ -29,10 +31,11 @@ class Log:
             #添加到hanler
             self.logger.addHandler(fh)
             self.logger.addHandler(ch)
-
+            '''
             #关闭通道
             self.logger.removeHandler(fh)
             self.logger.removeHandler(ch)
+            '''
     @property
     def fmt(self):
         return '%(levelname)s\t%(asctime)s\t[%(filename)s:%(lineno)d]\t%(message)s'
@@ -40,5 +43,5 @@ class Log:
 log = Log().logger
 
 if __name__ == '__main__':
-    log.info('hello word!')
-    log.removeHandler()
+    log.debug('hello word!')
+    #log.removeHandler()
