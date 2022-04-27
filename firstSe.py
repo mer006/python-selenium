b7a9b3cab5d2d83f754b4464a0e7e64425cc4253
mer006-2 <798481537@qq.com>
2022-04-27 18:32:34 +0800

    frame内层和外层的切换

--- firstSe.py
+++ firstSe.py
@@ -4,7 +4,7 @@ from time import sleep
 from selenium.webdriver.common.by import By
 import re
 
-
+#ec
 def firefoxDriver():
     server = webdriver.Firefox()
     server.maximize_window()
@@ -22,13 +22,31 @@ def firefoxDriver():
     server.find_element(By.XPATH,"//*[@id='_easyui_tree_6']").click()
     #print(server.page_source)
     print(server.current_url)
+    sleep(2)
 
     #定位到iframe
     iframe = server.find_element(By.XPATH,"//*[@id='_panel_/SalePerformance/BasePerformance/Index']/iframe")
     server.switch_to.frame(iframe)
     print(server.current_window_handle)
-    print(server.page_source)
+    #print(server.page_source)
     server.find_element(By.XPATH, "//*[@id='_easyui_textbox_input2']").send_keys('ZH06')
+
+    #定位回到主层
+    server.switch_to.default_content()
+    sleep(2)
+    #server.find_element(By.XPATH,"//*[div='绩效管理']").click()
+    server.find_element(By.ID,"logout").click()
+    sleep(3)
+    #退出登录
+    server.find_element(By.XPATH,"//span[span='确定']").click()
+    sleep(3)
+    server.quit()
+
+#baidu
+def firefoxDriverBaidu():
+    server = webdriver.Firefox()
+    server.maximize_window()
+    server.get("https://www.baidu.com")
 
     sleep(3)
     server.quit()
---
+++ test.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+####运算符  // % **
+print(3**2) #3的平方
+print(9//3)
+
